16d44fccb878a3a2336cb072450e6780e423ec64
Naveen Kumar <naveen.kumar@disney.com>
2021-11-03 20:43:46 -0700

    added new endpoint for updating payload with multiple entries

--- serve.py
+++ serve.py
@@ -23,23 +23,58 @@ def load_image_db(path):
     img_feats = np.array([a["ftr"] for a in d["hash"]])
     fnames = [a["filename"] for a in d["hash"]]
 
-@app.route("/search", methods=['GET','POST'])
-def query_images():
+@app.route("/update", methods=['POST'])
+def update_json_payload():
+    '''
+    Example payload:
+    {
+            "StoryWritingJobStatus": “a string”, # IN_PROGRESS | FAILED | COMPLETED
+            "Data": [
+            {
+                “id”: 0,
+                “pageType”: “front”,    # front/body/end
+                “Text”: “input string”,
+            "MediaImgUris": ["s3://DOC-EXAMPLE-BUCKET/your-image-file"],
+            }...
+            ...
+            ...
+                    ]    
+    }
+    '''
+
+    payload = request.json
+    #Upload image uris with the query
+    for page in payload["Data"]:
+        t = page["Text"] 
+        top_imgs = get_topK_images(t, K=1)
+        page["MediaImgUris"] = [a[1]  for a in top_imgs]
+
+    return payload
+
+
+def get_topK_images(query, K=1):
     global model, device
-    query = request.args.get('text')
-    logging.info("Query:" + query)
-    text = clip.tokenize([query]).to(device)
 
     with torch.no_grad():
+        text = clip.tokenize([query]).to(device)
         text_features = model.encode_text(text).numpy()[0]
         logits_per_text = torch.FloatTensor(np.dot(img_feats, text_features))
         probs = logits_per_text.softmax(dim=-1).cpu().numpy()
         img_ind = probs.argsort()
-        ret = [(probs[i], fnames[i])  for i in img_ind[-3:]]
-        top_img = ret[-1][1]
+        ret = [(probs[i], fnames[i])  for i in img_ind]
+        topK_img = ret[-K:]
+        return topK_img
 
-        #Send the top image
-        return send_file("static/images/"+top_img)
+
+@app.route("/search", methods=['GET','POST'])
+def query_images():
+    query = request.args.get('text')
+    logging.info("Query:" + query)
+
+    top_img = get_topK_images(query)
+
+    #Send the top image
+    return send_file("static/images/"+top_img[0][1])
 
 
 if __name__ == "__main__":
