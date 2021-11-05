8f1ef4a27e9ac3f19f74cbae0b386074710131b4
Naveen Kumar <naveen.kumar@disney.com>
2021-11-05 11:14:27 -0700

    script to generate image features before packaging

---
+++ build_hash.py
@@ -0,0 +1,36 @@
+import torch
+import json
+import clip
+import numpy as np
+from PIL import Image
+import sys
+import glob
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+fnames = []
+d = {"hash" : []}
+n=0
+with torch.no_grad():
+
+    #iterate over images since it won't input batch
+    for img in sys.stdin:
+        img = img.rstrip()
+        img = '/'.join(img.split('/')[-2:])
+        print(img)
+        fnames.append(img)
+
+        image = preprocess(Image.open(img)).unsqueeze(0).to(device)
+        imf = model.encode_image(image)
+
+        dd = {"filename" : img, "ftr": imf[0].numpy().tolist()}
+        d["hash"].append(dd)
+        d["all_files"]=fnames
+
+        n+=1
+        if n%100 ==0:
+            print("Saving...")
+            with open("img-hash.json","w") as fout:
+                json.dump(d,fout)
+        
